a7d0eff085df6f1f95e67c848ad3029aa4a212f8
jmmaniego <jmmaniego@hotmail.com>
2016-11-14 04:38:25 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# TODO: create poll from user
+polls = [
+  "Who let the dogs out?",
+  "Do we need another social polling app?",
+  "Am I doing it right?"
+].map {|poll_text| {text: poll_text}}
+
+Poll.create(polls)
