ad9ad370ff88916d88174309da2cd5b1f99ba8d8
Juan Miguel Maniego <jmmaniego@Juans-MacBook-Pro.local>
2016-11-15 10:25:12 +0800

    add first and last name fields to users, rake db:migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+jm = User.find_or_create_by(email: 'jmmaniego@hotmail.com') do |user|
+  user.first_name = "Juan Miguel"
+  user.last_name  = "Maniego"
+  password = user.password = 'p@ssword',
+  user.password_confirmation = password
+end
+
 polls = [
   "Who let the dogs out?",
   "Do we need another social polling app?",
   "Am I doing it right?"
 ].map {|poll_text| {text: poll_text}}
-
-jm = User.first
 
 Poll.create(polls) do |poll|
   poll.user = jm
